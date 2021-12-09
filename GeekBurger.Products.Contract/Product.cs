049a5ef6206fdf988473ae8f242a6dc9630a7e33
Kennedy Paixão <kennedympaixao@gmail.com>
2021-12-09 20:52:35 -0300

    Add project files.

---
+++ GeekBurger.Products.Contract/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBurger.Products.Contract
+{
+    public class Product
+    {
+        public string StoreName { get; set; }
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public List<Item> Items { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class ProductChanged
+    {
+        public ProductState State { get; set; }
+        public Product Product { get; set; }
+    }
+
+    public enum ProductState
+    {
+        Deleted = 2,
+        Modified = 3,
+        Added = 4
+    }
+
+
+}
